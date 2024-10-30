06bffeef3f74ac8fb975bb33ce52b9f6bf09d92b
yakkomajuri <yakko.majuri@gmail.com>
2024-10-30 16:26:05 -0300

    chore: reformat, add more context to setup file

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="mlflow-custom-headers-plugin",
     version="1.0.0",
     description="MLFlow plugin that allows adding extra headers to requests to MLFlow in order to enable authentication and other use-cases.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Yakko Majuri",
     author_email="yakko.majuri@protonmail.com",
     license="MIT",
@@ -17,5 +22,9 @@ setup(
         "mlflow.request_header_provider": [
             "unused = mlflow_custom_headers_plugin.headers:CustomRequestHeaderProvider"
         ]
+    },
+    project_urls={
+        "Source": "https://github.com/username/mlflow_custom_headers_plugin",
+        "Bug Tracker": "https://github.com/username/mlflow_custom_headers_plugin/issues",
     },
 )
--- src/mlflow_custom_headers_plugin/headers.py
+++ src/mlflow_custom_headers_plugin/headers.py
@@ -12,13 +12,13 @@ class CustomRequestHeaderProvider(RequestHeaderProvider):
 
     def request_headers(self):
         request_headers = dict()
-        custom_headers_str = os.getenv('MLFLOW_CUSTOM_HEADERS', None)
-        
+        custom_headers_str = os.getenv("MLFLOW_CUSTOM_HEADERS", None)
+
         if not custom_headers_str:
-            return request_headers  
-        
-        for header_pair in custom_headers_str.split(','):
-            header_name, header_value = header_pair.split(':')
+            return request_headers
+
+        for header_pair in custom_headers_str.split(","):
+            header_name, header_value = header_pair.split(":")
             request_headers[header_name] = header_value
 
         return request_headers
